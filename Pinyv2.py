a88f9e77f1d3d4ae93594546864f4be27342cb8f
Karol Ostrowski <xlolsonx@gmail.com>
2017-04-02 12:50:00 +0200

    fixing syntax error in pinv2.py

--- Pinyv2.py
+++ Pinyv2.py
@@ -1,10 +1,5 @@
 from microbit import *
 import music
-pin0.write_analog(0)
 while True:
- 
-    while round(pin0.read_analog()*(3.3/1023),2) == 1.7:
-        display.show(Image.HAPPY)
-        sleep(1)
-    display.show(" ")
+    display.scroll(str(int(pin0.read_analog())*(3.3/1023)))
     
